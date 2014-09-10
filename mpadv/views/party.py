f88b2a85f984916ff9b2e7043936216ebfeb8ba3
legeorges <georges.labreche@gmail.com>
2014-09-10 04:39:01 -0700

    Initial implementation of aggregate for mps groups in party

--- mpadv/views/party.py
+++ mpadv/views/party.py
@@ -14,6 +14,19 @@ class Party(View):
         :param party_slug: slug value of the party.
         '''
 
+        party_declarations = self.get_aggregate_declarations(party_slug)
+
+        #pm_declarations = self.get_pm_declarations(party_slug)
+
+        return render_template(
+            'aggregate_declarations.html',
+            asset_declarations=party_declarations,
+            pm_asset_declarations=None)
+
+    def get_aggregate_declarations(self, party_slug):
+        ''' Get the aggregate declrations for the given Party.
+        :param party_slug: slug of the party name.
+        '''
         api_url = utils.get_api_url()
 
         request_url = "%s/aggregate/%s" % (api_url, party_slug)
@@ -23,5 +36,16 @@ class Party(View):
         asset_declarations = json.loads(response)
         ad_results = asset_declarations['result']
 
-        return render_template(
-            'aggregate_declarations.html', asset_declarations=ad_results)
+        return ad_results
+
+    def get_pm_declarations(self, party_slug):
+        api_url = utils.get_api_url()
+
+        request_url = "%s/%s" % (api_url, party_slug)
+
+        response = urlopen(request_url).read()
+
+        asset_declarations = json.loads(response)
+        ad_results = asset_declarations['result']
+
+        return ad_results
